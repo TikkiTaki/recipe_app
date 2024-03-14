22fbcfe64593673499f041fd31b32c0a3e2c9013
TikkiTaki <163309470+TikkiTaki@users.noreply.github.com>
2024-03-14 13:36:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,5 @@ from gui import*
 Screen = Gui()
 
 
-### Create website which contains recipes made through the GUI
-### Make the reipes rateable and a separate part of the website with top rated recipes
-### Napraviti exe file
-### Staviti na GIT
+
 
